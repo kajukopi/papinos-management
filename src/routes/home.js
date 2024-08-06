aeca52849edf7d2b416cc20cfeac985d746417c3
Kaju Kopi <kajukopi@gmail.com>
2024-08-06 09:43:06 +0700

    update commit

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,7 +1,8 @@
 const router = require("express").Router()
 
 router.get("/", (req, res, next) => {
-  res.locals = {title: "Hello world!", message: "This is express.js"}
+  res.locals = {title: "Hello world!", message: "This is express.js", scripts: ["index"]}
+  console.log(res.locals)
   res.render("index")
 })
 
