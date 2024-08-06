696f5e711bf0c191b96bde7fd763e57d97a80ed2
Kaju Kopi <kajukopi@gmail.com>
2024-08-06 10:49:45 +0700

    update commit

--- src/routes/items.js
+++ src/routes/items.js
@@ -10,24 +10,24 @@ router.get("/", async (req, res) => {
   res.render("items", {items})
 })
 
-router.post("/items", async (req, res) => {
+router.post("/", async (req, res) => {
   const {name, description} = req.body
   const newItem = new Item({name, description})
   await newItem.save()
-  res.redirect("/items")
+  res.redirect("/")
 })
 
-router.put("/items/:id", async (req, res) => {
+router.put("/:id", async (req, res) => {
   const {id} = req.params
   const {name, description} = req.body
   await Item.findByIdAndUpdate(id, {name, description})
-  res.redirect("/items")
+  res.redirect("/")
 })
 
-router.delete("/items/:id", async (req, res) => {
+router.delete("/:id", async (req, res) => {
   const {id} = req.params
   await Item.findByIdAndDelete(id)
-  res.redirect("/items")
+  res.redirect("/")
 })
 
 module.exports = router
