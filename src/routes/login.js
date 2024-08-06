480c3218be5d60d05c516625bab4fb893541d8b5
Kaju Kopi <kajukopi@gmail.com>
2024-08-06 10:08:44 +0700

    update commit

--- src/routes/login.js
+++ src/routes/login.js
@@ -5,4 +5,10 @@ router.get("/", (req, res, next) => {
   res.render("login")
 })
 
+router.post("/", (req, res, next) => {
+  res.locals = {title: "Login Page", message: "This is express.js"}
+  const {username, password} = req.body
+  res.redirect(301, "/")
+})
+
 module.exports = router
--- src/routes/register.js
+++ src/routes/register.js
@@ -5,4 +5,10 @@ router.get("/", (req, res, next) => {
   res.render("register")
 })
 
+router.post("/", (req, res, next) => {
+  res.locals = {title: "Register Page", message: "This is express.js"}
+  const {username, password} = req.body
+  res.redirect(301, "/")
+})
+
 module.exports = router
