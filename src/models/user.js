9f0a6980f399960d7d9778f5bcb972d6b4152644
Kaju Kopi <kajukopi@gmail.com>
2024-08-10 10:39:38 +0700

    update

--- src/models/user.js
+++ src/models/user.js
@@ -4,6 +4,9 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
+    unique: true,
+    lowercase: true,
+    match: [/^\S+@\S+\.\S+$/, "Please provide a valid email address"],
   },
   password: {
     type: String,
